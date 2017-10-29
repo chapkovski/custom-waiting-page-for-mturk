1aa6f38628f4e3c2a21091a05eb91eb19826881d
chapkovski <chapkovski@gmail.com>
2017-10-29 03:21:33 +0100

    adding radiogrid template

--- settings.py
+++ settings.py
@@ -69,7 +69,7 @@ REAL_WORLD_CURRENCY_DECIMAL_PLACES = 3
 LANGUAGE_CODE = 'en'
 
 # if an app is included in SESSION_CONFIGS, you don't need to list it here
-INSTALLED_APPS = ['otree', 'otree_mturk_utils', 'radiogrid']
+INSTALLED_APPS = ['otree', 'otree_mturk_utils']
 
 # SENTRY_DSN = ''
 # SENTRY_DSN = 'http://2d6137799b914e1693146c5011f39030:46838e8caa374937a91b14b59ebbe164@sentry.otree.org/36'
